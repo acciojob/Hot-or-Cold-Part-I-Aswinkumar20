da613919a5f3dab490ad620e317ef20ec6719767
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-07-11 13:14:53 +0530

    updated content

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+let min = -20;
+let max = 20;
 
+// console.log("hello World")
+let number = document.getElementById("guess");
+let text = document.getElementById("num");
+
+function result(){
+    let randomNumber = Math.floor(Math.random()*(max-min+1)+min);
+    console.log(number.value);
+    console.log(randomNumber);
+    text.textContent = randomNumber;
+
+    if(randomNumber > (number.value - 5) && randomNumber < (number.value + 5)) {
+        text.textContent = "Hot";
+    } else {
+        text.textContent = "Cold";
+    }
+    
+}
